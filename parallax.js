24d7a793ba2244a165d54294d792b3c8614d4e29
Rabia-Rajput <rajput.rabia@outlook.com>
2024-02-23 16:36:20 +0100

    new changes made

--- parallax.js
+++ parallax.js
@@ -1,2 +1,5 @@
-const imageElement = document.getElementById('main-id');
-new simpleParallax(imageElement);//new is a key attribute to create javascript object based on blue prints
+const imageElement = document.getElementById("main-image");
+new simpleParallax(imageElement,{
+    scale:  1.6,
+    delay: 0.1,
+}); //new is a key attribute to create javascript object based on blue prints
